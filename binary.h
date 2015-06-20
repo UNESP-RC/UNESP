52d3e5ba2d7b771401a9462e5274a7f3483b07f1
Bruno Vedoveto Leandro <brunoovedoveto@gmail.com>
2015-06-20 11:33:42 -0300

    Arrumando funcao de binario para decimal

--- binary.h
+++ binary.h
@@ -32,11 +32,45 @@ void decimalToBinary(char* number, char* result, int tam)
     stringComplete(tam, result, result);
 }
 
+void undoTwoComplement(char* number){
+    if(number[0] == '1'){
+         int i=0, j=0;
+        
+         int firstOne=0;
+         int firstZero=0;
+        
+         for(i=0; i < strlen(number); i++){
+             if(number[i] == '0')
+               number[i] = '1';
+             else if(number[i] == '1')
+               number[i] = '0';
+         }
+        
+         for(i=0; i < strlen(number) && !firstOne; i++){
+             if(number[i] == '1'){
+                 firstOne = 1;
+            
+                 for(j=i; j < strlen(number) && !firstZero; j++){
+                     if(number[i] == '0'){
+                         firstZero = 1;
+                    
+                         number[i] = '1';
+                         number[i-1] = '0';
+                     }           
+                 }
+             }
+         }
+    }
+}
+
  // Função conversora: binário para decimal     
 void binaryToDecimal(char* number, char* result)
 {  
-   int aux, i, decimal, aux2; 
+   int aux, i, decimal, aux2, negative=0; 
    
+   if(number[0] == '1') negative = 1;
+   
+   undoTwoComplement(number);
    aux = strlen(number);
    i = 0;
    decimal = 0;
@@ -55,43 +89,9 @@ void binaryToDecimal(char* number, char* result)
         i++;
         aux--; 
    }
+   
+   if(negative)
+     decimal = (decimal+1) * -1;
       
    itoa(decimal, result, 10);   
 }
-
-//Função binario ---> complemento de dois 
-void comple2(char * number, char* result)
-{    int cont,bin, carry ;
-     cont = strlen(number)  ;
-     carry = 0;
-     while(cont >= 0)
-     {
-      if(number[cont] == '1')
-      {
-       result[cont] = '0';
-      }
-      else if(number[cont] == '0')
-           {
-            result[cont] = '1';
-           }
-
-      cont --;
-     }
-
-     cont = strlen(number) - 1 ;
-     do
-     {
-     if(result[cont] == '0')
-      {
-       result[cont] = '1';
-       carry = 0;
-       }else if(result[cont] == '1')
-            {
-             result[cont] = '0';
-             carry = 1;
-             cont --;
-            }
-
-     }while(carry != 0);
-
- }
